7c997a3a7f44ddbb68a4af9ec9ca07c61d30e25c
Орлов Ярослав <orlov.avis@yandex.ru>
2018-09-18 15:09:58 +0300

    commit from script

--- c/lab1/v2.c
+++ c/lab1/v2.c
@@ -1,10 +1,17 @@
 #include <stdoi.h>
+// Попарно поменять местами цифры, начиная с верхнего порядка. Если пары нет, поменять на ноль.
 
 int main(){
-    int n;
-    printf("Введите натуральное число n\n");
-    scanf("%d", n);
-    //if ( n <= 0) break;
-
+    int n, tmp;
+    while (1){
+		printf("Введите натуральное число n\n");
+		scanf("%d", &n);
+		//if ( n <= 0) break;
+		while ( n > 0){
+			tmp = n % 10;
+			printf("%d", tmp);
+			n /= 10;
+		}
+    }
     return 0;
 }
