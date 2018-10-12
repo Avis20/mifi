9f0e56168295367c151073618fbba61ea6190001
Student <b17v7114@PiNetDebian>
2018-10-12 21:54:44 +0300

    12-10-2018

--- c/lab2/v7-atan.c
+++ c/lab2/v7-atan.c
@@ -3,28 +3,26 @@
 
 int main(){
     int i, n;
-    double p, s, x;
-
-    // n = 5; x = 1;
-
+    double p, s, x, k;
+//    n = 100; x = 1;
     while(1){
 
         printf("Введите шаг n. Для выхода введите 0\n");
         scanf("%d", &n);
-        if (n < 0) break;
+        if (n == 0) break;
 
         printf("Введите вещественное число x\n");
         scanf("%lf", &x);
 
-        p = s = 1;
-        for (i = 1; i <= n; i++){
-            p = -p * x * x / ( (2*i-1)*(2*i) );
-            s += p;
-            // printf("i = %d; %f\n", i, p );
+        for (i = 2, s = p = x; i <= n; i++){
+            p *= -x * x;
+            k = p/(2*i-1);
+            s += k;
+            // printf("i = %d; p=%f; k=%f; s=%f\n", i, p, s );
         }
 
         printf("s = %lf\n", s);
-        printf("cos(%lf) = %lf\n\n", x, cos(x));
+        printf("atan(%lf) = %lf\n\n", x, atan(x));
 
     }
 
---
+++ c/lab3/v1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define N 100
+
+int input(int[], int[]);
+
+int main(){
+
+    int arr[N];
+
+    input();
+
+    return 0;
+}
+
+int input(int a[], int n){
+
+}
