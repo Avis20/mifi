593436ee53a54190b8c26e8349a8487ad5e879d4
ÐžÑ€Ð»Ð¾Ð² Ð¯Ñ€Ð¾ÑÐ»Ð°Ð² <orlov.avis@yandex.ru>
2019-03-22 17:11:05 +0300

    dsa

--- c2/lab1/v1.c
+++ c2/lab1/v1.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    char str[200];
-
-    scanf("%s", str );
-    printf("%s", str);
+    
+    int a; 
+    scanf("%d", &a);
+    printf("%d", a);
 
     return 0;
 }
