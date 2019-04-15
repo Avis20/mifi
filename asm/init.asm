aa7ff5ca537cd6f4f0593a30f20906c5030db79c
Орлов Ярослав <orlov.avis@yandex.ru>
2019-04-15 17:23:43 +0300

    commit from script

--- asm/init.asm
+++ asm/init.asm
@@ -1,10 +1,11 @@
 .model small
 .stack 100h
 .data
+; <-- your data
 .code
-    mov ax, @data
-    mov ax, 1
-    mov ds, ax
-    mov ax, 4c00h
+    mov AX, @data
+    mov DS, AX
+; <-- your code
+    mov AX, 4c00h
     int 21h
 end
