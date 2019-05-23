b2d59f6c2542531a1bc025d78183089f7c69ced8
Орлов Ярослав <orlov.avis@yandex.ru>
2019-05-23 09:25:17 +0300

    commit from script Чт май 23 09:25:17 MSK 2019

--- c2/lab3/main.c
+++ c2/lab3/main.c
@@ -1,7 +1,84 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+typedef struct item {
+    int busy;       // Флаг что у эл. с этим ключем есть данные
+    int key;        // Ключ по которому забираются данные
+    int release;    // Версия эл.
+    char *info;     // Сами данные
+} Item;
+
+typedef struct table {
+    int SZ;         // Размер вектора (?)
+    int n;          // Размер таблицы
+    Item *first;    // Первый эл. (?)
+    FILE *fd;       // Файл (?)
+} Table;
+
+int     D_Add(Table *);     // Добавление эл. в таблицу
+int     D_Find(Table *);    // Поиск в таб
+int     D_Delete(Table *);  // Удаление по ключу (?)
+int     D_Show(Table *);    // Вывод таблицы на экран
+
+int     dialog(const char *msgs[], int);
+int     get_int(int *);
+
+const char *msgs[] = {"0. Quit", "1. Add", "2. Find", "3. Delete", "4. Show"};
+const int NMsgs = sizeof(msgs) / sizeof(msgs[0]);
+
+int (*fptr[])(Table *) = {NULL, D_Add, D_Find, D_Delete, D_Show};
 
 int main(){
+    Table table = {0, 0, NULL, NULL};
+    int rc;
+
+    while(rc = dialog(msgs, NMsgs))
+        if(!fptr[rc](&table))
+            break;
+
+    printf("See you next time...\n");
 
     return 0;
 }
 
+int dialog(const char *msgs[ ], int N){
+    char *errmsg = "";
+    int rc;
+    int i, n;
+
+    do {
+        puts(errmsg);
+        errmsg = "You are wrong. Repeat, please!";
+        for (i = 0; i < N; ++i)
+            puts(msgs[i]);
+        
+        puts("Make your choice: --> ");
+        n = get_int(&rc);
+        if (n == 0) rc = 0;
+    } while(rc < 0 || rc >= N);
+
+    return rc;
+}
+
+
+int get_int(int *number){
+    int n;
+
+    do {
+        n = scanf("%d", number);
+        if ( n < 0 ) return 0;
+
+        if ( n == 0 ){
+            printf("Error: Repeat input: ");
+            scanf("%*[^\n]");
+        }
+
+    } while ( n == 0 );
+
+    return 1;
+}
+
+int D_Add(Table *t){return 1;};
+int D_Find(Table *t){return 1;};
+int D_Delete(Table *t){return 1;};
+int D_Show(Table *t){return 1;};
